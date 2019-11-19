2beb4c3f2ea3c400477111f6415a4fa66f1350ed
Diego <diego@pop-os.localdomain>
2019-11-19 23:34:30 +0100

    added config server

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-
 import dotenv from 'dotenv';
 dotenv.config();
 // usar https://stackoverflow.com/questions/50813591/how-to-use-node-config-in-typescript
@@ -11,8 +10,25 @@ import Server from './server';
 // import { resolvers } from "./resolvers/resolvers";
 
 function main() {
-    const server = Server.init(config.PORT);
-    server.start(() => {console.log("holita")});
+    const server = Server.init(config().PORT);
+    server.start(() => console.log('>> Running on ' + config().HOST + ':' + config().PORT));
+    // Pasar esta funcion con logger
+    // () => {
+    //     logger.info('Andsoft API');
+    //     logger.info('Current environment: ' + process.env.NODE_ENV);
+    //     logger.info('Server listening on port ' + config.PORT);
+    //     logger.info('Domain: ' + config.HOST)
+    // }
+
+    // Handle Errors
+    process.on('uncaughtException',function(err) {
+        console.log('Uncaught Exception:');
+        if (err.stack) {
+            console.log(err.stack);
+        } else {
+            console.log(err);
+        }
+    });
 }
 
 main();
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,22 +1,49 @@
 import express from 'express';
-import routes from '../routes';
+// import routes from '../routes';
 import morgan from 'morgan';
-
+import helmet from 'helmet';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+// BUENAS PRACTICAS https://expressjs.com/es/advanced/best-practice-security.html
 export default class Server {
     public app: express.Application;
 
     constructor(private port: number) {
         this.app = express();
         this.config();
-        this.app.use(routes);
+        // this.app.use(routes);
         this.app.use(morgan('dev'));
     }
 
-    // Load middlewares
+    // Load middlewares & config from file
     config() {
-        // cambiar process.env a configuracion
-        this.app.set('port', process.env.PORT || 3000);
-        this.app.use
+        this.app.use(helmet());
+        this.app.use(bodyParser.json({limit: '500kb'}));
+        this.app.use(bodyParser.urlencoded({limit: '500kb', extended: true}));
+        this.app.use(cors());
+
+        // // TODO: Add verbose flag
+        // if ((process.env.NODE_ENV || 'development') === 'development'){
+        //     const morgan = require('morgan');
+        //     const morganBody = require('morgan-body');
+        //     //app.use(morgan('combined'));
+        //     //app.use(morgan('dev', { 'stream': logger.stream }));
+        //     morganBody(app);
+        // }
+        // this.app.use(errorhandler());
+
+        // To server static files
+        this.app.use(express.static(__dirname + '/docs'));
+
+        // HTTPS AÑDIR HTTPS EN EXPRESS
+        // if (config.HTTPS) {
+        //     server = https.createServer(settings.https,function(req,res) {app(req,res);});
+        // } else {
+        //     server = http.createServer(function(req,res) {app(req,res);});
+        // }
+
+        this.app.setMaxListeners(0);
+        this.app.set('port', this.port || 3000);
     }
 
     routes() {
